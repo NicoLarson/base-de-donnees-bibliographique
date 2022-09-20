d260f5e210894a01c2db672e5256f320ac88aea3
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-09-20 11:38:43 -0300

    Add Convert CSV to JSON

---
+++ Convert_CSV_to_JSON/app.js
@@ -0,0 +1,80 @@
+const dataCsvInput = require("fs").readFileSync("./input/Ma bibliothèque.csv", "utf8")
+
+const lineBreak = dataCsvInput.split("\n")
+
+// Extraction des titres
+let header = lineBreak[0].split("\"")
+const haveCommas = (str) => {
+    return str != ','
+}
+header = header.filter(haveCommas)
+header.shift()
+header.pop()
+
+for (let i = 0; i < header.length; i++) {
+    header[i] = header[i].toLocaleLowerCase()
+    for (let j = 0; j < header[i].length; j++) {
+        if (header[i][j] == ' ') {
+            header[i] = header[i].replace(' ', '_')
+        }
+    }
+}
+// Traitement des données
+let data = []
+
+for (let i = 1; i < lineBreak.length; i++) {
+    let ligne = lineBreak[i].split("\"")
+    ligne = ligne.filter(haveCommas)
+    ligne.shift()
+    ligne.pop()
+    let document = new Object()
+    for (let j = 0; j < ligne.length; j++) {
+        if (header[j] == 'key' || header[j] == 'item_type' || header[j] == 'publication_year' || header[j] == 'author' || header[j] == 'title' || header[j] == 'publication_title' || header[j] == 'issn' || header[j] == 'doi' || header[j] == 'url' || header[j] == 'abstract_note' || header[j] == 'date_added' || header[j] == 'date_modified' || header[j] == 'pages' || header[j] == 'language' || header[j] == 'automatic_tags') {
+            // Traitement auteurs
+            if (header[j] == 'author') {
+                let arrayOfAuthors = []
+                ligne[j] = ligne[j].split(";")
+                for (let k = 0; k < ligne[j].length; k++) {
+                    if (ligne[j][k].includes(' ')) {
+                        ligne[j][k] = ligne[j][k].replace(' ', '')
+                    }
+                }
+                // Array of authors in string clean
+                for (let k = 0; k < ligne[j].length; k++) {
+                    let author = new Object()
+                    ligne[j][k] = ligne[j][k].split(',')
+                    author.firstName = ligne[j][k][0]
+                    author.lastName = ligne[j][k][1]
+                    arrayOfAuthors.push(author)
+                }
+                ligne[j] = arrayOfAuthors
+            }
+            // Traitement des tags
+            if (header[j] == 'automatic_tags') {
+                ligne[j] = ligne[j].split(';')
+                for (let k = 0; k < ligne[j].length; k++) {
+                    if (ligne[j][k].includes(' ')) {
+                        ligne[j][k] = ligne[j][k].replace(' ', '')
+                    }
+                }
+            }
+            document[header[j]] = ligne[j]
+            data.push(document)
+        }
+    }
+}
+
+// Clean duplicate items by title
+const uniqueDocument = Array.from(new Set(data.map(document => document.title)))
+    .map(title => {
+        return data.find(document => document.title === title)
+    })
+
+// Create JSON file
+
+const date = new Date();
+let month = date.getMonth() + 1
+let fileName = 'db' + date.getFullYear() + month + date.getDate() + date.getHours() + date.getMinutes() + '.json'
+const documents = { documents: uniqueDocument }
+let datas = JSON.stringify(documents)
+require("fs").writeFileSync(`./output/${fileName}`, datas)
--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# base de donnees bibliographique
- 
+# Base de données bibliographique
+
+Création d'une api pour accéder à des documents bibliographiques
+
+## 🏠 [Homepage](https://solamazon.com/data-base)
+
+## ✨ [Demo](https://nicolarson.github.io/periclim/)
+
+## Prérequis
+
+- Installer [Zotero + Connecteur chrome](https://www.zotero.org/download/)
+  - [Guide d'utilisation](https://www.zotero.org/support/quick_start_guide)
+  Logiciel de gestion bibliographiques
+- Avoir un compte [Heroku](https://www.heroku.com/)
+- Installer [NodeJS](https://nodejs.org/fr/)
+<!-- - NPM -->
+
+## Création d'une base de données noSQL (JSON)
+
+### Rechercher du contenus
+
+- Ouvrir le logiciel Zotero
+- Sur [sciencedirect.com](https://www.sciencedirect.com/)
+- Se connecter via le courriel institutionnel.
+- Utiliser les mots clés de recherche `guiana AND ("climate change" OR "global warning")`.
+- Cliquez sur l’icône du connecteur Zotero
+![Zotero connector icon](img/zotero-connector.png)
+
+- Une fenêtre apparaît
+- ![Fenêtre Zotero](img/zotero-connector-window.png)
+- Sélectionner les documents désirés puis cliquez sur OK
+- Les documents seront insérer dans le logiciel
+- > Vous pouvez supprimer les doublons via la fonction doublon du logiciel mais ce n'ai pas nécessaire
+
+### Exporter la bibliographie en CSV
+
+Dans `fichier` cliquez sur `Exporter la bibliothèque...`
+Sélectionnez le format CSV puis enregistrez le dans le dossier `Convert_CSV_to_JSON`.
+
+### Convertir le fichier CSV en JSON
+
+Allez dans le dossier `Convert_CSV_to_JSON`.
+Puis lancez la commande
+
+```bash
+node app.js 
+```
+
+L'application créera un fichier JSON nommé db puis la date de création, contenant un objet documents avec les bibliographies.
+
+## Creation d'une API open source
+
+## Installation
+
+```sh
+git push heroku main
+```
+
+## Mise à jour
+
+## Auteur
+
+👤 **Nicolas Yang**
+
+- Website: nicolasyang.dev
+- Github: [@nicolarson](https://github.com/nicolarson)
+- LinkedIn: [@nicolas-yang-dev](https://linkedin.com/in/nicolas-yang-dev)
